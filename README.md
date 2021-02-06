3699b26e70b686b092f35dcdb46f96d0c83d3e0e
tigelah <lokoportitimao2009@hotmail.com>
2021-02-06 17:23:09 -0300

    Added project CodePix in Golang

---
+++ README.md
@@ -0,0 +1,9 @@
+![Imersão Full Stack && Full Cycle](https://events-fullcycle.s3.amazonaws.com/events-fullcycle/static/site/img/grupo_4417.png)
+
+
+## Sobre o repositório
+Esse repositório é sobre aprendizagem no evento Full Stack && Full Cycle
+
+Nele contem um api que simula transações bancarias com Pix
+
+As instruções de instalações estão no README.md de cada projeto.
---
+++ codepix/domain/model/bank.go
@@ -0,0 +1,40 @@
+package model
+
+import (
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+	"time"
+)
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
+
+type Bank struct {
+	Base     `valid:"required"`
+	Code     string     `json:"code" gorm:"type:varchar(20)" valid:"notnull"`
+	Name     string     `json:"name" gorm:"type:varchar(255)" valid:"notnull"`
+	Accounts []*Account `gorm:"ForeignKey:BankID" valid:"-"`
+}
+
+func (bank *Bank) isValid() error {
+	_, err := govalidator.ValidateStruct(bank)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func NewBank(code string, name string) (*Bank, error) {
+	bank := Bank{
+		Code: code,
+		Name: name,
+	}
+	bank.ID = uuid.NewV4().String()
+	bank.CreatedAt = time.Now()
+	err := bank.isValid()
+	if err != nil {
+		return nil, err
+	}
+	return &bank, nil
+}
